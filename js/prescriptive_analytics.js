de3779b7e8d579a4a2b4b51885695890d2727c4e
PSeverina <severich@yandex.ru>
2019-06-26 15:58:05 +0300

    updates

--- js/prescriptive_analytics.js
+++ js/prescriptive_analytics.js
@@ -1,6 +1,16 @@
+$(document).ready(function () {
+    var small={width: "550px",height: "500px"};
+    var large={width: "650px",height: "600px"};
+    var count=1; 
+
+    $("#schema").on('click',function () { 
+        $(schema).animate((count==1)?large:small);
+        count = 1-count;
+    });
+});
+
 $(window).scroll(function(){
   if ($(document).scrollTop() > 50){
-      console.log('yes');
       $('.navbar').addClass('change');
   }
   else{
